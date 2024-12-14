49d7065be8a34c5d7e7feb2098e58d964ebe6d3e
AyoyH <ayah28603@gmail.com>
2024-12-14 04:59:04 +0200

    Edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
       fontFamily: {
         nav: ['var(--font-nav)'], // Map the CSS variable to Tailwind's font utility
       },
+      backgroundImage: {
+        "card-dark": "linear-gradient(120deg, #202020 0%, #050505 100%)",
+      },
     },
   },
   plugins: [],
